483315b10445b8b6a577c2ea35b54035d6502e30
alessia <alessia.profeta@gmail.com>
2024-10-27 14:49:32 +0100

    insert mini-project on clustre and k-means

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -43,8 +43,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -53,14 +54,30 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, f3 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
 
+# Definisci il numero di cluster
+k = 3  # Puoi modificare questo numero per esplorare diversi cluster
+
+# Crea il modello K-means e addestralo
+kmeans = KMeans(n_clusters=k, random_state=0)
+kmeans.fit(data)
+
+# Etichette di cluster per ogni punto dati
+pred = kmeans.labels_
+
+# Centroidi dei cluster
+centroids = kmeans.cluster_centers_
+
+print("Etichette dei cluster:", pred)
+print("Centroidi dei cluster:", centroids)
 
 
 ### rename the "name" parameter when you change the number of features
@@ -69,3 +86,25 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print("No predictions object named pred found, no clusters to plot")
+
+def get_max_min_by_values(value):
+    # Inizializziamo le variabili di massimo e minimo con valori estremi
+    max_value = float('-inf')
+    min_value = float('inf')
+
+    # Cicla su tutte le persone nel dizionario
+    for person, features in data_dict.items():
+        value_stock = features.get(value, "NaN")
+        
+        # Controlla che il valore non sia 'NaN'
+        if value_stock != 'NaN':
+            if value_stock > max_value:
+                max_value = value_stock
+            if value_stock < min_value:
+                min_value = value_stock
+
+    print(f"Max {value}:", max_value)
+    print(f"Min {value}:", min_value)
+    
+get_max_min_by_values("exercised_stock_options")
+get_max_min_by_values("salary")
